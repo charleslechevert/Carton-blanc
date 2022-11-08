33b5845eaf5057a8fdfb3c7b90a9fe495bb5f886
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-11-08 19:38:19 +0100

    check

--- app/database.js
+++ app/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
-const { DB_HOST, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env
+//const { DB_HOST, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env
 
 
 
 
-const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
+const sequelize = new Sequelize('d158ribaq37km9', 'qyhofagirwgfvs', '0b1fcce62a73b6c3e3c27cb8b93327560ebb8641d10cca45aa18c7a8ceb3da40', {
+  host: 'ec2-176-34-215-248.eu-west-1.compute.amazonaws.com',
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
