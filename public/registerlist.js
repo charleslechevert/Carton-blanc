038979d3cb0e58e038c1299cdcf9e92c5255bbd0
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-11-12 09:44:52 +0100

    fix buges

---
+++ public/registerlist.js
@@ -0,0 +1,32 @@
+const rows = document.querySelectorAll(".penaltylist__row")
+const paid = document.querySelectorAll(".registerlist__form-paidlogo")
+
+
+const search = document.querySelector('.registerlist__searchbar')
+console.log(search)
+search.addEventListener('keyup', () => {
+    const searchValue = search.value
+    for(row of rows) {
+        const isVisible = row.querySelector('.registerlist__joueur').innerText.includes(searchValue)  
+        row.classList.toggle('registerlist__hide-row', !isVisible)
+        
+    }
+})
+
+
+/*-----------------------------------------------------*/
+
+document.querySelector('.registerlist__paidfilter').addEventListener('click', () => {
+    document.querySelector('.registerlist__paidfilter').classList.toggle('registerlist__paidfilter--on')
+    let i = 0;
+
+    for(row of rows) {
+        if(paid[i].getAttribute('src') != 'paid.png' && row.querySelector('.registerlist__joueur').innerText.includes(search.value)  ) {
+            console.log('ok')
+            row.classList.toggle('registerlist__hide-row')
+        } else {
+            console.log('shey')
+        }
+        i++
+    }
+})
