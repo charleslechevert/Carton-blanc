13aae09357bf1051b164d85c2d05c67551c067e6
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-12-03 21:28:19 +0100

    fix bugs

--- app/controllers/playerController.js
+++ app/controllers/playerController.js
@@ -9,8 +9,8 @@ const playerController = {
 
   async create(req, res) {
     data = req.body;
-    data.active = true;
-    data.team_id = 37;
+    data.active = false;
+    data.team_id = '37c62bde-35cf-4e72-b61a-821d801877d0';
     console.log('CHECKKKK')
     console.log(data)
     // req.body contient les données de mon formulaire
